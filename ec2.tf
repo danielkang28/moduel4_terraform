bca4e3d570a6f4731a30a6f6487129e45eac9a9e
danielkang28 <danielkang28@gail.com>
2023-06-07 10:59:47 +0900

    ec2 file test

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,30 @@
 # ec2.tf
 
+# RSA 알고리즘을 이용해 private 키 생성.
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# private 키를 가지고 keypair 파일 생성.
+resource "aws_key_pair" "kp" {
+  key_name   = "ekskey"
+  public_key = tls_private_key.pk.public_key_openssh
+}
+
+# 키 파일을 생성하고 로컬에 다운로드.
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.kp.key_name}.pem"
+  content = tls_private_key.pk.private_key_pem
+}
 
 # 리소스 정의
 resource "aws_instance" "ubuntu_instance" {
-  ami                         = "ami-0c9c942bd7bf113a2" # Ubuntu 22.04
+  ami                         = "ami-0d52744d6551d851e" # Ubuntu 22.04
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.master-subnet.id
   associate_public_ip_address = true # Elastic IP 주소 자동 할당
-  key_name                    = "doo"
+  key_name                    = aws_key_pair.kp.key_name
 
 
   # connection {
